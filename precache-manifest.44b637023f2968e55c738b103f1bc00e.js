1272b33fc8a3bff369a74dcf5a7cd1777908d64f
LElliott242 <67988828+LElliott242@users.noreply.github.com>
2020-09-19 19:59:51 -0400

    Updates

---
+++ precache-manifest.44b637023f2968e55c738b103f1bc00e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30bca9fea8ef8b35ab9a04275037436c",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "916258372a753982c9d4",
+    "url": "/Ravenous/static/css/main.d02c0444.chunk.css"
+  },
+  {
+    "revision": "c128ee4e4d05d53cb7d0",
+    "url": "/Ravenous/static/js/2.bfd3eee6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous/static/js/2.bfd3eee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916258372a753982c9d4",
+    "url": "/Ravenous/static/js/main.5e895e6d.chunk.js"
+  },
+  {
+    "revision": "cd33480439a159f358bd",
+    "url": "/Ravenous/static/js/runtime-main.9bb2f265.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
